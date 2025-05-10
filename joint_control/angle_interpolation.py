7b0bda7add3eabf1f7fc5b7340bbeb617d27b346
gpkienzle <kienzle@campus.tu-berlin.de>
2025-05-10 23:02:56 +0200

    assignment02 angle interpolation

--- joint_control/angle_interpolation.py
+++ joint_control/angle_interpolation.py
@@ -35,13 +35,74 @@ class AngleInterpolationAgent(PIDAgent):
 
     def think(self, perception):
         target_joints = self.angle_interpolation(self.keyframes, perception)
-        target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
+        if 'LHipYawPitch' in target_joints:
+            target_joints['RHipYawPitch'] = target_joints['LHipYawPitch'] # copy missing joint in keyframes
         self.target_joints.update(target_joints)
         return super(AngleInterpolationAgent, self).think(perception)
 
     def angle_interpolation(self, keyframes, perception):
         target_joints = {}
         # YOUR CODE HERE
+        for i in range (len(keyframes[0])):
+            times = keyframes[1][i]
+
+            t = perception.time % keyframes[1][i][-1]
+            i_minus_1 = -1
+            for q in range (len(times)-1):
+                if times[q] < t and times[q+1] > t:
+                    i_minus_1 = q
+
+            angles = keyframes[2][i]
+            for a in range(len(angles)):
+                if type(angles[a]) == list:
+                    angles[a] = angles[a][0]
+            solution = [0, 0, 0, 0]
+            m0 = 0
+
+            if i_minus_1 == 0 or i_minus_1 == len(times)-1:
+                m1 = 0
+            else:
+                if (angles[i_minus_1+1]-angles[i_minus_1])*(angles[i_minus_1]-angles[i_minus_1-1]) < 0:
+                    m1 = 0
+                else:
+                    m1 = (angles[i_minus_1+1] - 2*angles[i_minus_1] + angles[i_minus_1-1])/2
+            if i_minus_1 == len(times)-1 or i_minus_1 == len(times)-2:
+                m0 = 0
+            else:
+                if (angles[i_minus_1+2]-angles[i_minus_1+1])*(angles[i_minus_1+1]-angles[i_minus_1]) < 0:
+                    m0 = 0
+                else:
+                    m0 = (angles[i_minus_1+2] - 2*angles[i_minus_1+1] + angles[i_minus_1])/2
+                
+            c = [[times[i_minus_1+1]**3, times[i_minus_1+1]**2, times[i_minus_1+1], 1],
+                     [times[i_minus_1]**3, times[i_minus_1]**2, times[i_minus_1], 1],
+                     [3*(times[i_minus_1+1]**2), 2*times[i_minus_1+1], 1, 0],
+                     [3*(times[i_minus_1]**2), 2*times[i_minus_1], 1, 0]]
+            f = [angles[i_minus_1+1], angles[i_minus_1], m1, m0]
+                
+            #Solving equation according to script of modul "Wissenschaftliches Rechnen"
+            for elem in range(4):
+                #Pivoting:
+                max_elem = elem
+                for element in range(elem, 4):
+                    if abs(c[element][elem]) > abs(c[max_elem][elem]):
+                        max_elem = element
+                    c[elem], c[max_elem] = c[max_elem], c[elem]
+                    f[elem], f[max_elem] = f[max_elem], f[elem]
+                #Elimination:
+                for row in range(elem+1, 4):
+                    fac = c[row][elem]/c[elem][elem]
+                    for column in range(elem, 4):
+                        c[row][column] -= fac*c[elem][column]
+                    f[row] -= fac*f[elem]
+
+            for k in range(3, -1, -1):
+                values_sum = 0
+                for l in range(k+1, 4):
+                    values_sum += c[k][l]*solution[l]
+                solution[k] = (f[k] - values_sum)/c[k][k]
+            func_val = solution[0]*t**3+solution[1]*t**2+solution[2]*t+solution[3]
+            target_joints.update({keyframes[0][i]: func_val})
 
         return target_joints
 
